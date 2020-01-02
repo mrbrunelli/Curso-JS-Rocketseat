f28580fd98086622553979e4e8eab26b3dd82a44
Matheus Ricardo Brunelli <matheus.brunelli@gmail.com>
2020-01-02 20:11:38 -0300

    consumindo api com sucesso

--- ajax.js
+++ ajax.js
@@ -1,1 +1,10 @@
 var xhr = new XMLHttpRequest()
+
+xhr.open('GET', 'https://api.github.com/users/mrbrunelli')
+xhr.send(null)
+
+xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4) {
+        console.log(JSON.parse(xhr.responseText))
+    }
+}
